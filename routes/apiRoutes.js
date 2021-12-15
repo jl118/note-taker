386855681aef6ddae56bb96765a752e935068287
jl118 <jen.liebelt@gmail.com>
2021-12-14 19:00:52 -0800

    fixed issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,39 @@
 const path = require("path");
 const express = require("express");
-const { readFromFile, readAndAppend, readAndDelete } = require("../helpers/fsUtils.js");
+const {
+  readFromFile,
+  readAndAppend,
+  readAndDelete,
+} = require("../helpers/fsUtils.js");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 
-
-router.get("/api/notes", (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+router.get("/notes", (req, res) => {
+    console.log("get notes");
+  readFromFile("./db/db.json").then((data) => {
+    console.log("this is all notes" + data);
+    res.json(JSON.parse(data));
+  });
 });
 
-router.post("/api/notes", (req, res) => {
-    const {title, text} = req.body;
-    if (req.body){
-        const newNote = {
-            title,
-            text,
-            id: uuidv4()
-        };
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
 
-        readAndAppend(newNote, "./db/db.json");
-        readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
-    }
+    readAndAppend(newNote, "./db/db.json");
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  }
 });
 
-router.delete("/api/notes/:id", (req, res) => {
-    const {id} = req.params;
-    readAndDelete(id, "./db/db.json");
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+router.delete("/notes/:id", (req, res) => {
+  const { id } = req.params;
+  readAndDelete(id, "./db/db.json");
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
@@ -11,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
